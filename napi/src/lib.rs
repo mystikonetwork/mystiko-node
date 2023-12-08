0a50da13979283fc3ee4e12a132d2b72ff7e3b13
ericlurker <96100006+ericlurker@users.noreply.github.com>
2023-12-08 10:31:55 +0800

    update, fix workflow error

--- napi/src/lib.rs
+++ napi/src/lib.rs
@@ -1,13 +1,5 @@
 #![deny(clippy::all)]
 
-use napi::bindgen_prelude::Buffer;
-use napi_derive::napi;
-use prost::Message;
-
-
-// pub mod config;
-// pub mod handler;
-// pub mod mystiko;
-
-#[napi(constructor)]
-pub struct Test;
+pub mod config;
+pub mod handler;
+pub mod mystiko;
